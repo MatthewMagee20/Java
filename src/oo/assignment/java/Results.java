927e58a4c657167a4ff7faabc0bb78c6ba9e1c48
matthewmagee20 <c17456744@mydit.ie>
2019-04-13 14:20:22 +0100

    text file to lowercase and stop words removal working

--- src/oo/assignment/java/Results.java
+++ src/oo/assignment/java/Results.java
@@ -1,6 +1,8 @@
 package oo.assignment.java;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Scanner;
 import javax.swing.*;
 import java.awt.BorderLayout;
@@ -86,12 +88,10 @@ public class Results extends JFrame implements ActionListener, MouseListener
 					e.printStackTrace();
 				}
 			
-				
-				
-			
+					
 			ArrayList<String> words = new ArrayList<String>();
 			ArrayList<Integer> count = new ArrayList<Integer>();
-			
+
 			while(fileInput.hasNext())
 			{
 				String nextWord = fileInput.next();
@@ -114,6 +114,7 @@ public class Results extends JFrame implements ActionListener, MouseListener
 			for(int i = 0; i < words.size(); i ++)
 			{
 				
+
 				System.out.println("____________________________________");
 				System.out.println("     "+words.get(i)+" = " + count.get(i)+ " time/s");
 		
@@ -127,11 +128,95 @@ public class Results extends JFrame implements ActionListener, MouseListener
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}		
+			}
 			
+			String[] stopWords = {"a", "as", "able", "about",
+					"above", "according", "accordingly", "across", "actually",
+					"after", "afterwards", "again", "against", "aint", "all",
+					"allow", "allows", "almost", "alone", "along", "already",
+					"also", "although", "always", "am", "among", "amongst", "an",
+					"and", "another", "any", "anybody", "anyhow", "anyone", "anything",
+					"anyway", "anyways", "anywhere", "apart", "appear", "appreciate",
+					"appropriate", "are", "arent", "around", "as", "aside", "ask", "asking",
+					"associated", "at", "available", "away", "awfully", "be", "became", "because",
+					"become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
+					"believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both",
+					"brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant", "cause", "causes",
+					"certain", "certainly", "changes", "clearly", "co", "com", "come",
+					"comes", "concerning", "consequently", "consider", "considering", "contain",
+					"containing",    "contains","corresponding","could", "couldnt", "course", "currently",
+					"definitely", "described", "despite", "did", "didnt", "different", "do", "does",
+					"doesnt", "doing", "dont", "done", "down", "downwards", "during", "each", "edu",
+					"eg", "eight", "either", "else", "elsewhere", "enough", "entirely", "especially",
+					"et", "etc", "even", "ever", "every", "everybody", "everyone", "everything", "everywhere",
+					"ex", "exactly", "example", "except", "far", "few", "ff", "fifth", "first", "five", "followed",   
+					"following", "follows", "for", "former", "formerly", "forth", "four", "from", "further",
+					"furthermore", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone",
+					"got", "gotten", "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have",
+					"havent", "having", "he", "hes", "hello", "help", "hence", "her", "here", "heres", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", 
+					"i", "id", "ill", "im", "ive", "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate", "indicated", "indicates", "inner", "insofar", "instead", "into", "inward", "is", "isnt", "it", "itd", "itll", "its", "its", "itself", 
+					"just", 
+					"keep", "keeps", "kept", "know", "knows", "known", 
+					"last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd", 
+					"mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most", "mostly", "much", "must", "my", "myself",
+					"name", "namely", "nd", "near", "nearly", "necessary", "need", "needs", "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor", "normally", "not", "nothing", "novel", "now", "nowhere",
+					"obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own",
+					"particular", "particularly", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provides", 
+					"que", "quite", "qv", 
+					"rather", "rd", "re", "really", "reasonably", "regarding", "regardless", "regards", "relatively", "respectively", "right", 
+					"said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "she", "should", "shouldnt", "since", "six", "so", "some", "somebody", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", 
+					"ts", "take", "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "thats", "thats", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "theres", "thereafter", "thereby", "therefore", "therein", "theres", "thereupon", "these", "they", "theyd", "theyll", "theyre", "theyve", "think", "third", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "twice", "two", 
+					"un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up", "upon", "us", "use", "used", "useful", "uses", "using", "usually",
+					"value", "various", "very", "via", "viz", "vs", 
+					"want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve", "welcome", "well", "went", "were", "werent", "what", "whats", "whatever", "when", "whence", "whenever", "where", "wheres", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whos", "whoever", "whole", "whom", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wont", "wonder", "would", "would", "wouldnt", 
+					"yes", "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves", 
+					"zero"};
+			
+			//ArrayList<String> remStop = new ArrayList<>(Arrays.asList(stopWords));
+			
+			ArrayList<String> words = new ArrayList<String>();
+
+			while(fileInput.hasNext())
+			{
+				String nextWord = fileInput.next().toLowerCase();
+				
+				if(words.contains(nextWord))
+				{
+					int i = words.indexOf(nextWord);
+					
+				}
+				else {
+					words.add(nextWord);
+					
+				}
+			
+			}
+			
+			 for (int j = 0; j < stopWords.length; j++) {
+	                if (words.contains(stopWords[j])) {
+	                    words.remove(stopWords[j]);//remove it
+	                }
+	            }
+			
+			fileInput.close();
+
+			for(int i = 0; i < words.size(); i ++)
+			{
+				
+
+				System.out.println("____________________________________");
+				System.out.println("     "+words.get(i));
+				
+			}
 		}
-	}
-		
+			}
+	
+
+			
+
+
+
+			
 	public File getFile() {
 		return file;
 	}
